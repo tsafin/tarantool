af1fe92397dc4388c642ad29794fe1cad66cc7e7
Timur Safin <tsafin@tarantool.org>
2022-01-14 03:38:15 +0300

    temp - bad formatting in tnt_datetime_parse_full wrapping

--- src/lua/tnt_datetime.c
+++ src/lua/tnt_datetime.c
@@ -34,7 +34,7 @@ tnt_datetime_to_string(const struct datetime *date, char *buf, ssize_t len)
 
 size_t
 tnt_datetime_parse_full(struct datetime *date, const char *str, size_t len,
-		    int32_t offset)
+			int32_t offset)
 {
 	return datetime_parse_full(date, str, len, offset);
 }
