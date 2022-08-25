ad6c3b689361de2ce4e9c8f3dfdc1ac93366c6ba
Timur Safin <tsafin@tarantool.org>
2022-08-25 18:07:47 +0300

    debugger: luatest for console debugger
    Created luatest test for interactive debugger luadebug.lua.
    We use separate debug-target.lua for execution under
    control of debugger session.
    
    NO_DOC=test
    NO_CHANGELOG=test

---
+++ test/app-luatest/console_debugger_session_test.lua
@@ -0,0 +1,59 @@
+local t = require('luatest')
+local popen = require('popen')
+local g = t.group()
+
+local function normalize_path(s)
+    return s:gsub("^@", ""):gsub("[^/]+$", "")
+end
+
+local function unescape(s)
+    return s:gsub('[\27\155][][()#;?%d]*[A-PRZcf-ntqry=><~]', '')
+end
+
+local TARANTOOL_PATH = arg[-1]
+local path_to_script = normalize_path(debug.getinfo(1, 'S').source)
+local debug_target_script = path_to_script .. 'debug-target.lua'
+
+local DEBUGGER = 'luadebug.lua'
+local dbg_header = DEBUGGER .. ": Loaded for " .. jit.version
+local dbg_prompt = DEBUGGER .. '>'
+
+local sequence = {
+    { [''] = dbg_header },
+    { ['n\n'] = dbg_prompt },
+    { ['n\n'] = dbg_prompt },
+    { ['p T\n'] = 'T => 1970-01-01T03:00:00+0300' },
+    { ['n\n'] = dbg_prompt },
+    { ['n\n'] = dbg_prompt },
+    { ['n\n'] = dbg_prompt },
+    { ['n\n'] = dbg_prompt },
+    { ['p S\n'] = 'S => "1970-01-01T0300+0300"' },
+    { ['n\n'] = dbg_prompt },
+    { ['n\n'] = dbg_prompt },
+    { ['p T1\n'] = 'T1 => 1970-01-01T03:00:00+0300' },
+    { ['n\n'] = dbg_prompt },
+    { ['n\n'] = '' },
+}
+
+g.test_interactive_debugger_session = function()
+    local cmd = { TARANTOOL_PATH, debug_target_script }
+    local fh = popen.new(cmd, {
+        stdout = popen.opts.PIPE,
+        stderr = popen.opts.DEVNULL,
+        stdin = popen.opts.PIPE,
+    })
+    t.assert_is_not(fh, nil)
+    for _, row in pairs(sequence) do
+        local cmd, expected = next(row)
+        if cmd ~= '' then
+            fh:write(cmd)
+        end
+        local result = unescape(fh:read({ timeout = 1.0 }))
+        -- dirty hack to skip empty output if there is only command returned
+        if result == cmd then
+            result = unescape(fh:read({ timeout = 1.0 }))
+        end
+        t.assert_str_contains(result, expected, false)
+    end
+    fh:close()
+end
