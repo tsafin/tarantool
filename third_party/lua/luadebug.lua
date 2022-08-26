d7a982486566b44afdd9aaaab5ac9e09d7839e39
Timur Safin <tsafin@tarantool.org>
2022-08-26 16:22:49 +0300

    debugger: cleanup code
    Get rid of all irrelevant for the Tarantool code:
    - branches of code for non-LuaJIT interpreter;
    - LuaJIT, but Windows-specific code;
    - Commented out code, which is incompatible with Tarantool console.
    
    NO_DOC=refactoring
    NO_CHANGELOG=refactoring
    NO_TEST=refactoring

--- third_party/lua/luadebug.lua
+++ third_party/lua/luadebug.lua
@@ -667,32 +667,10 @@ function dbg.msgh(...)
     return ...
 end
 
--- Assume stdin/out are TTYs unless we can use LuaJIT's FFI to properly check
--- them.
---local stdin_isatty = true
-local stdout_isatty = true
+local ffi = require("ffi")
+ffi.cdef [[ int isatty(int); ]]
 
--- Conditionally enable the LuaJIT FFI.
-local ffi = (jit and require("ffi"))
-if ffi then
-    ffi.cdef [[
-        int isatty(int); // Unix
-        int _isatty(int); // Windows
-        void free(void *ptr);
-
-        char *readline(const char *);
-        int add_history(const char *);
-    ]]
-
-    local function get_func_or_nil(sym)
-        local success, func = pcall(function() return ffi.C[sym] end)
-        return success and func or nil
-    end
-
-    local isatty = get_func_or_nil("isatty") or get_func_or_nil("_isatty")
-    -- stdin_isatty = isatty(0)
-    stdout_isatty = isatty(1)
-end
+local stdout_isatty = ffi.C.isatty(1)
 
 -- Conditionally enable color support.
 local color_maybe_supported = (stdout_isatty and os.getenv("TERM") and
@@ -706,83 +684,10 @@ if color_maybe_supported and not os.getenv("DBG_NOCOLOR") then
     GREEN_CARET = string.char(27) .. "[92m => " .. COLOR_RESET
 end
 
---[[
-if stdin_isatty and not os.getenv("DBG_NOREADLINE") then
-    pcall(function()
-        local linenoise = require 'linenoise'
-
-        -- Load command history from ~/.lua_history
-        local hist_path = os.getenv('HOME') .. '/.lua_history'
-        linenoise.historyload(hist_path)
-        linenoise.historysetmaxlen(50)
-
-        local function autocomplete(env, input, matches)
-            for name, _ in pairs(env) do
-                if name:match('^' .. input .. '.*') then
-                    linenoise.addcompletion(matches, name)
-                end
-            end
-        end
-
-        -- Auto-completion for locals and globals
-        linenoise.setcompletion(function(matches, input)
-            -- First, check the locals and upvalues.
-            local env = local_bindings(1, true)
-            autocomplete(env, input, matches)
-
-            -- Then, check the implicit environment.
-            env = getmetatable(env).__index
-            autocomplete(env, input, matches)
-        end)
-
-        dbg.read = function(prompt)
-            local str = linenoise.linenoise(prompt)
-            if str and not str:match "^%s*$" then
-                linenoise.historyadd(str)
-                linenoise.historysave(hist_path)
-            end
-            return str
-        end
-        dbg_writeln(COLOR_YELLOW.."luadebug.lua: "..COLOR_RESET.."Linenoise support enabled.")
-    end)
-
-    -- Conditionally enable LuaJIT readline support.
-    pcall(function()
-        if dbg.read == nil and ffi then
-            local readline = ffi.load("readline")
-            dbg.read = function(prompt)
-                local cstr = readline.readline(prompt)
-                if cstr ~= nil then
-                    local str = ffi.string(cstr)
-                    if string.match(str, "[^%s]+") then
-                        readline.add_history(cstr)
-                    end
-
-                    ffi.C.free(cstr)
-                    return str
-                else
-                    return nil
-                end
-            end
-            dbg_writeln(COLOR_YELLOW.."luadebug.lua: "..COLOR_RESET.."Readline support enabled.")
-        end
-    end)
-end
---]]
-
--- Detect Lua version.
-if jit then -- LuaJIT
-    LUA_JIT_SETLOCAL_WORKAROUND = -1
-    dbg_writeln(COLOR_YELLOW .. "luadebug.lua: " .. COLOR_RESET ..
-                "Loaded for ".. jit.version)
-    jit.off()
-elseif "Lua 5.1" <= _VERSION and _VERSION <= "Lua 5.4" then
-    dbg_writeln(COLOR_YELLOW .. "luadebug.lua: " .. COLOR_RESET ..
-                "Loaded for " .. _VERSION)
-else
-    dbg_writeln(COLOR_YELLOW .. "luadebug.lua: " .. COLOR_RESET ..
-                "Not tested against " .. _VERSION)
-    dbg_writeln("Please send me feedback!")
-end
+-- Detect Lua/LuaJIT version.
+LUA_JIT_SETLOCAL_WORKAROUND = -1
+dbg_writeln(COLOR_YELLOW .. "luadebug.lua: " .. COLOR_RESET .. "Loaded for " ..
+            jit.version)
+jit.off()
 
 return dbg
