7a40f6dd192cfdf0103d92485cbee61d7cb7e33f
Timur Safin <tsafin@tarantool.org>
2022-12-15 02:24:13 +0300

    debugger: refactor debugging scenarios
    Refactor the way how we run various debugger scenarios, getting rid
    of external static file, and producing debuggee scripts on the fly.
    The idea is to have everything in the single source: both source
    script to be used for debugging, and corresponding debugger commands.
    
    NO_DOC=internal
    NO_CHANGELOG=internal

--- test/app-luatest/console_debugger_session_test.lua
+++ test/app-luatest/console_debugger_session_test.lua
@@ -1,10 +1,7 @@
 local t = require('luatest')
 local popen = require('popen')
 local tnt = require('tarantool')
-
-local function normalize_path(s)
-    return s:gsub("^@", ""):gsub("[^/]+$", "")
-end
+local fio = require('fio')
 
 local function unescape(s)
     return s and s:gsub('[\27\155][][()#;?%d]*[A-PRZcf-ntqry=><~]', '') or ''
@@ -22,9 +19,6 @@ local function tarantool_path(arg)
 end
 
 local TARANTOOL_PATH = tarantool_path(arg)
-local path_to_script = normalize_path(debug.getinfo(1, 'S').source)
-local debug_target_script = path_to_script .. 'debug-target.lua'
-
 local DEBUGGER = 'luadebug'
 local dbg_header = "Tarantool debugger " .. tnt.version
 local dbg_prompt = DEBUGGER .. '>'
@@ -39,6 +33,46 @@ local function debuglog(...)
         return
     end
     print(...)
+end
+
+--[[
+    Convert passed multi-line literal script
+    to the temporary file. Get rid of line annotations,
+    leaving code alone.
+
+    Annotated lines are in a form:
+        1 | local date = require 'datetime'
+        2 | local T = date.new{hour = 3, tzoffset = '+0300'}
+]]
+local function dbg_script(content)
+    local tmpname = os.tmpname()
+    local f = assert(io.open(tmpname, "wb"))
+    -- process multi-line literal
+    for line in string.gmatch(content, "[^\n]+") do
+        --[[
+            Process annotated code line, delimited with '|'.
+
+        +-------- virtual line number (for easier maintaining)
+        | +------ delimiter
+        | | +---- Lua code to be executed
+        V V V
+        1 | local date = require 'datetime'
+
+        ]]
+        local delim = line:find('|')
+
+        -- if there is no delimiter - write it as-is
+        if not delim then
+            f:write(line..'\n')
+        else
+            -- TODO - assert if there is incorrect line numbers
+            -- at the moment just ignore annotation entirely.
+            f:write(line:sub(delim + 1)..'\n')
+        end
+    end
+    f:close()
+    debuglog("temporary name:", tmpname)
+    return tmpname
 end
 
 local cmd_aliases = {
@@ -91,6 +125,20 @@ local sequences = {
 
     -- full, complex sequence
     full = {
+        dbg_script [[
+     1|local date = require 'datetime'
+     2|
+     3|local T = date.new{hour = 3, tzoffset = '+0300'}
+     4| print(T)
+     5|
+     6|local fmt = '%Y-%m-%dT%H%M%z'
+     7|local S  = T:format(fmt)
+     8|print(S)
+     9|local T1 = date.parse(S, {format = fmt})
+    10|print(T1)
+    11|
+    12|os.exit(0)
+]],
         { ['\t'] = '' }, -- \t is a special value for start
         { ['n'] = dbg_prompt },
         { ['s'] = dbg_prompt },
@@ -103,11 +151,11 @@ local sequences = {
         { ['w'] = 'local hms = false' },
         { ['h'] = dbg_prompt },
         { ['t'] = '=> builtin/datetime.lua' },
-        { ['u'] = 'debug-target.lua:3 in chunk at' },
+        { ['u'] = '{file}:3 in chunk at' },
         -- FIXME - we should not show calling side at luadebug.lua::start
         -- { ['u'] = 'Already at the bottom of the stack.' },
         { ['u'] = 'Inspecting frame: builtin/luadebug.lua' },
-        { ['d'] = 'debug-target.lua:3 in chunk at' },
+        { ['d'] = '{file}:3 in chunk at' },
         { ['d'] = 'Inspecting frame: builtin/datetime.lua' },
         { ['l'] = 'obj => {"tzoffset" = "+0300", "hour" = 3}' },
         { ['f'] = dbg_prompt },
@@ -123,9 +171,23 @@ local sequences = {
 
     -- partial sequence with successful breakpoints added
     bps_ok = {
+        dbg_script [[
+     1|local date = require 'datetime'
+     2|
+     3|local T = date.new{hour = 3, tzoffset = '+0300'}
+     4|print(T)
+     5|
+     6|local fmt = '%Y-%m-%dT%H%M%z'
+     7|local S  = T:format(fmt)
+     8|print(S)
+     9|local T1 = date.parse(S, {format = fmt})
+    10|print(T1)
+    11|
+    12|os.exit(0)
+]],
         { ['\t'] = '' }, -- \t is a special value for start
         { ['b +9'] = dbg_prompt },
-        { ['c'] = 'debug-target.lua:9' },
+        { ['c'] = '{file}:9' },
         { ['n'] = dbg_prompt },
         { ['p S'] = 'S => "1970-01-01T0300+0300"' },
         { ['p T'] = 'T => 1970-01-01T03:00:00+0300' },
@@ -134,42 +196,72 @@ local sequences = {
 
     -- partial sequence with failed breakpoint addsitions
     bps_fail = {
+        dbg_script [[
+    1|local date = require 'datetime'
+    2|local T = date.new{hour = 3, tzoffset = '+0300'}
+    3|print(T)
+    4|os.exit(0)
+]],
         { ['\t'] = '' }, -- \t is a special value for start
         { ['b'] = 'command expects argument, but none received' },
         { ['b 11'] = dbg_failed_bp },
-        { ['b debug-target.lua'] = dbg_failed_bp },
-        { ['b debug-target.lua:'] = dbg_failed_bp },
-        { ['b debug-target.lua:-10'] = dbg_failed_bp },
+        { ['b {file}'] = dbg_failed_bp },
+        { ['b {file}:'] = dbg_failed_bp },
+        { ['b {file}:-10'] = dbg_failed_bp },
     },
 
     -- partial sequence with breakpoints additions and removals
     bpd_ok = {
+        dbg_script [[
+     1|local date = require 'datetime'
+     2|
+     3|local T = date.new{hour = 3, tzoffset = '+0300'}
+     4|print(T)
+     5|
+     6|local fmt = '%Y-%m-%dT%H%M%z'
+     7|local S  = T:format(fmt)
+     8|print(S)
+     9|local T1 = date.parse(S, {format = fmt})
+    10|print(T1)
+    11|
+    12|os.exit(0)
+]],
         { ['\t'] = '' }, -- \t is a special value for start
-        { ['b +7'] = 'debug-target.lua:7' },
-        { ['bl'] = 'debug-target.lua:7' },
-        { ['b :5'] = 'debug-target.lua:5' },
-        { ['bl'] = 'debug-target.lua:5' },
-        { ['bd :5'] = 'debug-target.lua:5' },
-        { ['bl'] = 'debug-target.lua:7' },
-        { ['bd +7'] = 'debug-target.lua:7' },
-        { ['b :6'] = 'debug-target.lua:6' },
-        { ['bl'] = 'debug-target.lua:6' },
+        { ['b +7'] = '{file}:7' },
+        { ['bl'] = '{file}:7' },
+        { ['b :5'] = '{file}:5' },
+        { ['bl'] = '{file}:5' },
+        { ['bd :5'] = '{file}:5' },
+        { ['bl'] = '{file}:7' },
+        { ['bd +7'] = '{file}:7' },
+        { ['b :6'] = '{file}:6' },
+        { ['bl'] = '{file}:6' },
         { ['bd *'] = 'Removed all breakpoints' },
         { ['bl'] = 'No active breakpoints defined' },
     },
     -- partial sequence with failed breakpoints removals
     bpd_fail = {
+        dbg_script [[
+    1|local date = require 'datetime'
+    2|local T = date.new{hour = 3, tzoffset = '+0300'}
+    3|print(T)
+    4|os.exit(0)
+]],
         { ['\t'] = '' }, -- \t is a special value for start
         { ['bd'] = 'command expects argument, but none received' },
         { ['bd 11'] = dbg_failed_bpd },
-        { ['bd debug-target.lua'] = dbg_failed_bpd },
-        { ['bd debug-target.lua:'] = dbg_failed_bpd },
-        { ['bd debug-target.lua:-10'] = dbg_failed_bpd },
+        { ['bd {file}'] = dbg_failed_bpd },
+        { ['bd {file}:'] = dbg_failed_bpd },
+        { ['bd {file}:-10'] = dbg_failed_bpd },
     },
 }
 
 local function debug_session(sequence)
-    local cmd = { TARANTOOL_PATH, '-d', debug_target_script }
+    local tmpfile = table.remove(sequence, 1)
+    t.assert(type(tmpfile) == 'string')
+    local cmd = { TARANTOOL_PATH, '-d', tmpfile }
+    local short_src = fio.basename(tmpfile)
+    debuglog('short_src:', short_src)
     --[[
         repeat multiple times to check all command aliases
     ]]
@@ -183,6 +275,8 @@ local function debug_session(sequence)
         local first = true
         for _, row in pairs(sequence) do
             local cmd, expected = next(row)
+            -- interpret template strings {file}
+            expected = expected:gsub('{file}', short_src)
             if cmd ~= '\t' then
                 if cmd ~= '' then
                     local key, arg = get_key_arg_pair(cmd)
@@ -218,6 +312,8 @@ local function debug_session(sequence)
         end
         fh:close()
     end
+    debuglog('delete ', tmpfile)
+    os.remove(tmpfile)
 end
 
 g.test_interactive_debugger_session = function(cg)
